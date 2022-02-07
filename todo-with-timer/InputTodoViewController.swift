9d21c570e8216d4adeb19be60c0b29d51bf6e81a
hecuhecu <hecunyuji5@gmail.com>
2022-02-07 22:51:07 +0900

    first commit

---
+++ todo-with-timer/InputTodoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InputTodoViewController.swift
+//  todo-with-timer
+//
+//  Created by 河村宇記 on 2022/02/07.
+//
+
+import UIKit
+
+class InputTodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
